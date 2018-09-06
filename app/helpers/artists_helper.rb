615a5bff8f851646d77c7d58ef10486f2419b144
Nancy Jo <enciie@gmail.com>
2018-09-06 01:47:59 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,8 +3,9 @@ module ArtistsHelper
   def display_artist(song) #take in a song object
     if song.artist
       linkt_to @song.artist.name, song.artist
-    
-    
+    else
+      link_to 'Add Artist', edit_song_path(song)
+    end
   end
 
 end
